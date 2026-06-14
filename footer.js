9b5b1c06642e36cd1b8e61e68188a5cc7266b92d
Mansigndu <mansi.saini2412@gmail.com>
2026-06-14 16:44:21 +0530

    do some changes

--- footer.js
+++ footer.js
@@ -224,7 +224,7 @@ footer.innerHTML = `
 
 <a href="https://wa.me/919326609636"
     target="_blank"
-    class="fixed bottom-6 right-6 w-16 h-16 bg-green-500 rounded-full flex items-center justify-center text-white text-3xl shadow-2xl z-50 hover:scale-110 transition duration-300">
+    class="fixed bottom-16 right-6 w-16 h-16 bg-green-500 rounded-full flex items-center justify-center text-white text-3xl shadow-2xl z-50 hover:scale-110 transition duration-300">
 
     <i class="fab fa-whatsapp"></i>
 
